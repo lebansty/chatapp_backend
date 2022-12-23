5f2d4fc243bc49c101af976e08498e1ab2074ede
lebansty <lebanstyv@gmail.com>
2022-12-23 09:39:44 +0530

    initial

---
+++ index.js
@@ -0,0 +1,38 @@
+const express =require('express');
+const app= express();
+const http = require('http');
+const cors= require('cors');
+const {Server} = require('socket.io')
+app.use(cors());
+
+//to link the socket to the express
+const server =http.createServer(app);
+
+
+const io=new Server(server,{
+    cors:{
+        orgin:"https://jazzy-piroshki-a5258a.netlify.app",
+        methods:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log(`User Connected: ${socket.id}`);
+
+  socket.on('join_room',(data)=>{
+    socket.join(data);
+    console.log(socket.id,data)
+  })
+
+    socket.on("send_messege",(data)=>{
+        socket.to(data.room).emit("recieve_messege",data)
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected: "+socket.id)
+    })
+})
+
+server.listen(3001,()=>{
+    console.log("Server is running....")
+})
