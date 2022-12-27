2407965ac706983b54e3d486bfe48835e3eef50f
lebansty <lebanstyv@gmail.com>
2022-12-27 11:33:41 +0530

    render

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ require('dotenv').config()
 const http = require('http');
 const cors= require('cors');
 const {Server} = require('socket.io')
-app.use(cors({
-    origin:'https://jazzy-piroshki-a5258a.netlify.app'
-}))
+app.use(cors())
 
 //to link the socket to the express
 const server =http.createServer(app);
 
 
-const io=new Server(server)
+const io=new Server(server,{
+  cors:{
+    orgin:"https://jazzy-piroshki-a5258a.netlify.app",
+    methods:["GET","POST"]
+  }
+})
 
 io.on("connection",(socket)=>{
     console.log(`User Connected: ${socket.id}`);
